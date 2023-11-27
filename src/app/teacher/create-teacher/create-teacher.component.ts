1d5d06c520e5c94b8a376ad9632fa62a65d3fb4c
Cezar Cruz <cezarcruz22@gmail.com>
2023-11-27 07:42:10 -0300

    adding news

--- src/app/teacher/create-teacher/create-teacher.component.ts
+++ src/app/teacher/create-teacher/create-teacher.component.ts
@@ -1,11 +1,113 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  AbstractControl,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { PanelModule } from 'primeng/panel';
+import { TeacherService } from '../../core/services/teacher.service';
+import { ToastService } from '../../core/services';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { Teacher } from '../../core/models/teacher';
 
 @Component({
   selector: 'app-create-teacher',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    PanelModule,
+    ButtonModule,
+    InputTextModule,
+  ],
   templateUrl: './create-teacher.component.html',
   styleUrl: './create-teacher.component.scss',
 })
-export class CreateTeacherComponent {}
+export class CreateTeacherComponent {
+  private readonly fb = inject(NonNullableFormBuilder);
+  private readonly teacherService = inject(TeacherService);
+  private readonly toastService = inject(ToastService);
+
+  protected isEditing: boolean = false;
+  protected editingId: number = 0;
+
+  protected teacherForm = this.fb.group({
+    name: ['', Validators.required],
+  });
+
+  @Output()
+  public saveFinished = new EventEmitter<void>();
+
+  @Output()
+  public cancelEvent = new EventEmitter<void>();
+
+  @Input({ required: true })
+  set teacher(teacher: Teacher | undefined) {
+    if (teacher) {
+      this.editingId = teacher.id;
+      this.isEditing = true;
+      this.teacherForm.patchValue(teacher);
+    }
+  }
+
+  protected submit() {
+    if (this.teacherForm.invalid) {
+      this.teacherName.markAsDirty({ onlySelf: true });
+      console.error('not today');
+      return;
+    }
+
+    if (this.isEditing && this.editingId != 0) {
+      this.teacherService
+        .update(this.teacherForm.getRawValue(), this.editingId)
+        .subscribe((body) => {
+          console.log(body);
+          this.resetEditing();
+          this.saveFinished.emit();
+          this.toastService.showSuccessMessage(
+            'Professor atualizado com sucesso',
+          );
+        });
+      return;
+    }
+
+    this.teacherService
+      .create(this.teacherForm.getRawValue())
+      .subscribe((body) => {
+        console.log(body);
+        this.resetEditing();
+        this.toastService.showSuccessMessage(
+          'Professor cadastrador com sucesso',
+        );
+
+        this.saveFinished.emit();
+      });
+  }
+
+  protected remove() {
+    this.teacherService.remove(this.editingId).subscribe((body) => {
+      console.log(body);
+      this.resetEditing();
+      this.saveFinished.emit();
+      this.toastService.showSuccessMessage('Professor removido com sucesso');
+    });
+  }
+
+  protected cancel() {
+    this.resetEditing();
+    this.cancelEvent.emit();
+  }
+
+  private resetEditing() {
+    this.teacherForm.reset();
+    this.editingId = 0;
+    this.isEditing = false;
+  }
+
+  get teacherName() {
+    return this.teacherForm.get('name') as AbstractControl;
+  }
+}
--- src/app/teacher/teacher-manager/teacher-manager.component.ts
+++ src/app/teacher/teacher-manager/teacher-manager.component.ts
@@ -2,21 +2,16 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PanelModule } from 'primeng/panel';
-import {
-  AbstractControl,
-  NonNullableFormBuilder,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TeacherService } from '../../core/services/teacher.service';
 import { Teacher } from '../../core/models/teacher';
 import { TableModule } from 'primeng/table';
-import { ToastService } from '../../core/services';
 import { MessagesModule } from 'primeng/messages';
 import { Message } from 'primeng/api';
+import { CreateTeacherComponent } from '../create-teacher/create-teacher.component';
 
 @Component({
   selector: 'app-teacher-manager',
@@ -29,19 +24,16 @@ import { Message } from 'primeng/api';
     InputTextModule,
     TableModule,
     MessagesModule,
+    CreateTeacherComponent,
   ],
   templateUrl: './teacher-manager.component.html',
   styleUrl: './teacher-manager.component.scss',
 })
 export class TeacherManagerComponent implements OnInit {
-  private readonly fb = inject(NonNullableFormBuilder);
   private readonly teacherService = inject(TeacherService);
-  private readonly toastService = inject(ToastService);
 
   protected teachers: Teacher[] = [];
-
-  protected isEditing: boolean = false;
-  protected editingId: number = 0;
+  protected teacherToEdit: Teacher | undefined;
 
   protected messages: Message[] = [
     {
@@ -50,10 +42,6 @@ export class TeacherManagerComponent implements OnInit {
       detail: 'Nenhum Professor cadastrado, que tal comećar agora?',
     },
   ];
-
-  protected teacherForm = this.fb.group({
-    name: ['', Validators.required],
-  });
 
   ngOnInit(): void {
     this.getAllTeachers();
@@ -65,61 +53,15 @@ export class TeacherManagerComponent implements OnInit {
       .subscribe((teachers) => (this.teachers = teachers));
   }
 
-  protected submit() {
-    if (this.teacherForm.invalid) {
-      this.teacherName.markAsDirty({ onlySelf: true });
-      console.error('not today');
-      return;
-    }
-
-    if (this.isEditing && this.editingId != 0) {
-      this.teacherService
-        .update(this.teacherForm.getRawValue(), this.editingId)
-        .subscribe((body) => {
-          console.log(body);
-          this.resetEditing();
-          this.getAllTeachers();
-          this.toastService.showSuccessMessage(
-            'Professor atualizado com sucesso',
-          );
-        });
-      return;
-    }
-
-    this.teacherService
-      .create(this.teacherForm.getRawValue())
-      .subscribe((body) => {
-        console.log(body);
-        this.teacherForm.reset();
-        this.getAllTeachers();
-        this.toastService.showSuccessMessage(
-          'Professor cadastrador com sucesso',
-        );
-      });
-  }
-
-  protected remove() {
-    this.teacherService.remove(this.editingId).subscribe((body) => {
-      console.log(body);
-      this.resetEditing();
-      this.getAllTeachers();
-      this.toastService.showSuccessMessage('Professor removido com sucesso');
-    });
-  }
-
   protected rowEditInit(teacher: Teacher) {
-    this.isEditing = true;
-    this.editingId = teacher.id;
-    this.teacherForm.patchValue(teacher);
+    this.teacherToEdit = teacher;
   }
 
-  private resetEditing() {
-    this.teacherForm.reset();
-    this.editingId = 0;
-    this.isEditing = false;
+  protected onCancel() {
+    this.teacherToEdit = undefined;
   }
 
-  get teacherName() {
-    return this.teacherForm.get('name') as AbstractControl;
+  protected saveFinished() {
+    this.getAllTeachers();
   }
 }
