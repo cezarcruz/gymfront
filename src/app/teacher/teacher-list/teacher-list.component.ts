92137a955576fb63868a66246fb22dd7257628b2
Cezar Cruz <cezarcruz22@gmail.com>
2023-12-08 18:38:14 -0300

    wip

---
+++ src/app/teacher/teacher-list/teacher-list.component.ts
@@ -0,0 +1,47 @@
+import { Component, EventEmitter, OnInit, Output, inject } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { TeacherService } from '../../core/services';
+import { Observable } from 'rxjs';
+import { Teacher } from '../../core/models/teacher';
+import { MessagesModule } from 'primeng/messages';
+import { AsyncPipe } from '@angular/common';
+import { Message } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-teacher-list',
+  standalone: true,
+  imports: [TableModule, MessagesModule, AsyncPipe, ButtonModule],
+  templateUrl: './teacher-list.component.html',
+  styleUrl: './teacher-list.component.scss',
+})
+export class TeacherListComponent implements OnInit {
+  private readonly teacherService = inject(TeacherService);
+
+  protected teachers$ = new Observable<Teacher[]>();
+
+  protected messages: Message[] = [
+    {
+      severity: 'info',
+      summary: 'Info',
+      detail: 'Nenhum Professor cadastrado, que tal começar agora?',
+    },
+  ];
+
+  @Output()
+  private edit = new EventEmitter<Teacher>();
+
+  ngOnInit(): void {
+    this.getAllTeachers();
+  }
+
+  protected getAllTeachers() {
+    console.log('getting all teachers');
+
+    this.teachers$ = this.teacherService.getAll();
+  }
+
+  protected rowEditInit(teacher: Teacher) {
+    this.onEdit.emit(teacher);
+  }
+}
--- src/app/teacher/teacher-manager/teacher-manager.component.ts
+++ src/app/teacher/teacher-manager/teacher-manager.component.ts
@@ -1,18 +1,14 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { AsyncPipe, CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { PanelModule } from 'primeng/panel';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
-import { TeacherService } from '../../core/services/teacher.service';
 import { Teacher } from '../../core/models/teacher';
-import { TableModule } from 'primeng/table';
-import { MessagesModule } from 'primeng/messages';
-import { Message } from 'primeng/api';
 import { CreateTeacherComponent } from '../create-teacher/create-teacher.component';
-import { Observable } from 'rxjs';
+import { TeacherListComponent } from '../teacher-list/teacher-list.component';
 
 @Component({
   selector: 'app-teacher-manager',
@@ -23,45 +19,28 @@ import { Observable } from 'rxjs';
     PanelModule,
     ReactiveFormsModule,
     InputTextModule,
-    TableModule,
-    MessagesModule,
     CreateTeacherComponent,
-    AsyncPipe,
+    TeacherListComponent,
   ],
   templateUrl: './teacher-manager.component.html',
   styleUrl: './teacher-manager.component.scss',
 })
-export class TeacherManagerComponent implements OnInit {
-  private readonly teacherService = inject(TeacherService);
-
-  protected teachers$ = new Observable<Teacher[]>();
+export class TeacherManagerComponent {
   protected teacherToEdit: Teacher | undefined;
 
-  protected messages: Message[] = [
-    {
-      severity: 'info',
-      summary: 'Info',
-      detail: 'Nenhum Professor cadastrado, que tal começar agora?',
-    },
-  ];
-
-  ngOnInit(): void {
-    this.getAllTeachers();
-  }
-
-  protected getAllTeachers() {
-    this.teachers$ = this.teacherService.getAll();
-  }
-
-  protected rowEditInit(teacher: Teacher) {
-    this.teacherToEdit = teacher;
-  }
+  protected editing = false;
 
   protected onCancel() {
+    this.editing = false;
     this.teacherToEdit = undefined;
   }
 
+  onEdit($event: Teacher) {
+    this.editing = true;
+    this.teacherToEdit = $event;
+  }
+
   protected saveFinished() {
-    this.getAllTeachers();
+    this.editing = false;
   }
 }
